6287eed036112ccbd18284103184f18f1be6c9b1
rs222kn <rs222kn@student.lnu.se>
2016-11-02 13:19:48 +0100

    From array like

--- observable.js
+++ observable.js
@@ -8,18 +8,36 @@ const Rx = require('rxjs/Rx');
 
 
 // from Event
-const emitterStream = Rx.Observable.fromEvent(myEmitter, 'data');
+const emitterStream = Rx.Observable.fromEvent(myEmitter, 'data')
+  .map(a => a.message);
 
 emitterStream.subscribe(
-  (data) => {
+  data => {
     console.log('data', data);
   },
-  (e) => {
+  e => {
     console.log('error', e);
   },
-  (completed) => {
-    console.log(completed);
+  complete => {
+    console.log('completed');
   });
 
-
 myEmitter.emit('data', { message: 'hello world'});
+
+
+// from array like
+
+const numbers = [1, 20, 343, 32, 22, 9];
+
+const numbers$ = Rx.Observable.from(numbers);
+
+numbers$.subscribe(
+  data => {
+    console.log(data);
+  },
+  e => {
+    console.log('error', e);
+  },
+  complete => {
+    console.log('completed');
+  });
